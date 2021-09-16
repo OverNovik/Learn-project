7c6c05c34c54bc88f217b9399f586ffd3e70f6bc
Aliaxander Novikov <nerevarin11@gmail.com>
2021-09-16 20:07:44 +0300

    create learn project

---
+++ react-quiz/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Layout from "./hoc/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <h1>Hello world!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
