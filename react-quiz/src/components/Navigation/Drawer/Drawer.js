3863c9ca74af6cab8863f0cc666b23e643e92a74
Aliaxander Novikov <nerevarin11@gmail.com>
2021-09-22 11:54:40 +0300

    add react-router-dom

--- react-quiz/src/components/Navigation/Drawer/Drawer.js
+++ react-quiz/src/components/Navigation/Drawer/Drawer.js
@@ -10,7 +10,7 @@ class Drawer extends Component {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <a href="#!">Link {link}</a>
         </li>
       )
     })
